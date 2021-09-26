21d728dbf9c47f00a14efb977b39c27254eca022
jeohyn <chajhyeon@gmail.com>
2021-09-26 17:11:29 +0900

    edit stepSum calculate prob

--- stopIt/ViewModels/StepCountViewModel.swift
+++ stopIt/ViewModels/StepCountViewModel.swift
@@ -8,8 +8,10 @@ class StepCountViewModel: ObservableObject {
     @Published var stepSum: [StepSum] = []
     
     init() {
-        for _ in 0..<7 {
-            self.stepSum.append(StepSum(date: Date(), steps: 0))
+        for i in -7 ... -1{//지난 7일간의 휴식시간 구하기
+            let today = Date()
+            let day = Calendar.current.date(byAdding: .day, value: i, to: today)!
+            self.stepSum.append(StepSum(date: day, steps: 0))
         }
     }
     
@@ -21,11 +23,17 @@ class StepCountViewModel: ObservableObject {
                 
                 StepCountModel.shared.makeDayStepData()
                 
-                self.stepSum+=StepCountModel.shared.stepSum
+                //self.stepSum=StepCountModel.shared.stepSum
+
+                for step in StepCountModel.shared.stepSum{
+                    let date=step.date
+                    let target = Calendar.current.dateComponents([.day], from:date, to:Date())
+                    self.stepSum[6-(target.day!-1)].steps+=step.steps
+                }
             }
         })
-        
 
     }
     
 }
+
